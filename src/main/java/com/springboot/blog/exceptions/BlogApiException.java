8f669e82282905ec8ed8847171025faca6ae110d
amit6897 <amit.jangra21@outlook.com>
2021-12-03 17:28:29 +0530

    spring boot blog rest api

---
+++ src/main/java/com/springboot/blog/exceptions/BlogApiException.java
@@ -0,0 +1,30 @@
+package com.springboot.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+// we throw this exception whenever we write some business logic or validate request parameters
+public class BlogApiException extends RuntimeException {
+
+    private HttpStatus status;
+    private String message;
+
+    public BlogApiException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public BlogApiException(String message, HttpStatus status, String message1) {
+        super(message);
+        this.status = status;
+        this.message = message1;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
